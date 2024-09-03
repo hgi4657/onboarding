7c1f8664dc68fd841c3ab3d0337e6adc62ca2663
hgi4657 <hgi4657@naver.com>
2024-09-04 01:04:17 +0900

    feat: 권한 정보를 List 형태로 반환하기 위해 추가

--- src/main/java/com/sparta/onboarding/user/entity/User.java
+++ src/main/java/com/sparta/onboarding/user/entity/User.java
@@ -8,12 +8,16 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import java.util.List;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 
 @Entity
 @Table(name = "users")
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,7 +26,7 @@ public class User {
     @Column(nullable = false, unique = true, length = 30)
     private String username;
 
-    @Column(nullable = false, length = 50)
+    @Column(nullable = false, length = 254)
     private String password;
 
     @Column(nullable = false, length = 20)
@@ -31,4 +35,15 @@ public class User {
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private Role role;
+
+    public User(String username, String password, String nickname, Role role) {
+        this.username = username;
+        this.password = password;
+        this.nickname = nickname;
+        this.role = role;
+    }
+
+    public List<Role> getRoles() {
+        return List.of(this.role);
+    }
 }
