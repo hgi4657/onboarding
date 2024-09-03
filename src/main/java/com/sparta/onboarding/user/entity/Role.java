81bea18bb5a49ba0c7d95d4b7d0dbfc3bc974110
hgi4657 <hgi4657@naver.com>
2024-09-03 10:29:30 +0900

    feat: user, role 파일 추가

---
+++ src/main/java/com/sparta/onboarding/user/entity/Role.java
@@ -0,0 +1,6 @@
+package com.sparta.onboarding.user.entity;
+
+public enum Role {
+    ROLE_USER,
+    ROLE_ADMIN
+}
---
+++ src/main/java/com/sparta/onboarding/user/entity/User.java
@@ -0,0 +1,34 @@
+package com.sparta.onboarding.user.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+
+
+@Entity
+@Table(name = "users")
+@Getter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 30)
+    private String username;
+
+    @Column(nullable = false, length = 50)
+    private String password;
+
+    @Column(nullable = false, length = 20)
+    private String nickname;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+}
