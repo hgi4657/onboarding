e9d219b1e3bb70dec403b581eae36d8056337854
hgi4657 <hgi4657@naver.com>
2024-09-03 17:48:37 +0900

    feat: 통일을 위한 ErrorResponse 추가 및 GlobalExceptionHandler 추가

---
+++ src/main/java/com/sparta/onboarding/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.sparta.onboarding.common.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<ErrorResponse> handleCustomException(CustomException ex) {
+        ErrorResponse errorResponse = new ErrorResponse(ex.getStatus().value(), ex.getMessage());
+        return ResponseEntity.status(ex.getStatus()).body(errorResponse);
+    }
+}
